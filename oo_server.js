86f706b26e7c7c12a8092fcd18cd7625672271ca
Apurv Srivasta <apurvsrivastav@outlook.com>
2022-06-06 05:15:37 -0700

    setup basic module and env

---
+++ oo_server.js
@@ -0,0 +1,15 @@
+console.log("starting the server")
+//const http = require('http');
+const express = require('express')
+const dotenv = require('dotenv')    // why is this one used?
+
+dotenv.config({ path: './config/config.env'});
+
+const express_app = express();
+
+const PORT = process.env.PORT || 5550;
+express_app.listen(
+      PORT,
+      console.log(`Server running in ${process.env.NODE_ENBV} mode on port ${PORT}`)
+);
+console.log('bye tatae');
